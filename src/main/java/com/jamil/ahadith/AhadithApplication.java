6e8accf5bc31a5020bc2054d5e8842d8285ca680
Jamil Helal <jamil.helal.37@gmail.com>
2026-06-17 22:31:45 +0300

    first commit

---
+++ src/main/java/com/jamil/ahadith/AhadithApplication.java
@@ -0,0 +1,13 @@
+package com.jamil.ahadith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhadithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AhadithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jamil/ahadith/AhadithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamil.ahadith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhadithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
